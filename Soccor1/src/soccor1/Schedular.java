c97c027b335996467c8cd8abaaa076b0ac642906
Mr. Wilson <bwilson@macon.k12.mo.us>
2019-12-06 09:26:07 -0600

    Second

---
+++ Soccor1/src/soccor1/Schedular.java
@@ -0,0 +1,29 @@
+
+package soccor1;
+
+
+class Schedular {
+
+    void newSeason() {
+    
+        // Instantiate 3 Classes - Team, Game, PrintControler
+        
+        Team my_Teams = new Team();
+        
+        Game my_Game = new Game();
+        
+        PrintControler my_Print = new PrintControler();
+        
+        
+        
+        
+        
+        
+        
+    
+    
+    
+    
+    }
+    
+}
